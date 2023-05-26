6efe5530a58e968c01b6ccc434917aa745406f8a
Aeminaydemir <86062969+Aeminaydemir@users.noreply.github.com>
2023-05-26 20:01:34 +0300

    Add files via upload

---
+++ Pratik_sortNumbers/src/SortNumbers.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+public class SortNumbers {
+    public static void main(String[] args) {
+        int a,b,c;
+        Scanner in=new Scanner(System.in);
+        System.out.print("First number a:");
+        a=in.nextInt();
+        System.out.print("Second number b:");
+        b=in.nextInt();
+        System.out.print("Third number c:");
+        c=in.nextInt();
+        if (a>b && a>c) {
+            if (b>c){
+                System.out.println("    c<b<a");
+            }else if(c>b) {
+                System.out.println("    b<c<a");
+            }else {
+                System.out.println("    c=b<a");
+            }
+        } else if (b>c && b>a) {
+            if (a>c){
+                System.out.println("    c<a<b");
+            }else if(c>a){
+                System.out.println("    a<c<b");
+            }else {
+                System.out.println("    c=a<b");
+            }
+        }else if (c>a && c>b) {
+            if (a>b){
+                System.out.println("    b<a<c");
+            }else if (b>a){
+                System.out.println("    a<b<c");
+            }else {
+                System.out.println("    a=b<c");
+            }
+        }else {
+
+            if (a==b && b!=c) {
+                System.out.println("    c<a=b");
+            } else if (a==c && a!=b) {
+                        System.out.println("    b<a=c");
+            } else if (b != a) {
+                System.out.println("    a<b=c");
+                }else {
+                System.out.println("    a=b=c");
+            }
+            }
+        }
+    }
+
